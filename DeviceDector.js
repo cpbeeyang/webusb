744958105cc3637127e96ad7f2293ea5718a2450
cpbeeyang <50903449+cpbeeyang@users.noreply.github.com>
2019-05-22 16:13:44 +0800

    Update DeviceDector.js

--- DeviceDector.js
+++ DeviceDector.js
@@ -8,17 +8,17 @@
 
 var USBDevice = {
 
-    requestDevicePermission: function() {
-        navigator.usb.requestDevice({ filters: [{ vendorId: 0x0483, productId: 0x5710 },
-                { vendorId: 0x0630, productId: 0x0818 },
-                { vendorId: 0x0630, productId: 0x0618 },
-                { vendorId: 0x0630, productId: 0x0818 }] })
-            .then(device => {
-                console.log(device.productName);
-                console.log(device.manufacturerName);
-            })
-            .catch(error => { console.log(error); });
-    },
+// requestDevicePermission: function() {
+    //     navigator.usb.requestDevice({ filters: [{ vendorId: 0x0483, productId: 0x5710 },
+    //             { vendorId: 0x0630, productId: 0x0818 },
+    //             { vendorId: 0x0630, productId: 0x0618 },
+    //             { vendorId: 0x0630, productId: 0x0818 }] })
+    //         .then(device => {
+    //             console.log(device.productName);
+    //             console.log(device.manufacturerName);
+    //         })
+    //         .catch(error => { console.log(error); });
+    // },
 
     getDevice: function() {
         return window.navigator.usb.getDevices().then((devices) => {
@@ -26,7 +26,7 @@ var USBDevice = {
         })
     }
 }
-USBDevice.requestDevicePermission();
+// USBDevice.requestDevicePermission();
 
 function registerUsbEventListeners () {
     console.log("reader connect");
